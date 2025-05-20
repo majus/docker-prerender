9a2a6aaf3c21f011737a7fd015f4fa12ae84ff2d
Denis <denis.perov@gmail.com>
2025-05-21 00:19:17 +0300

    Remove memory cache & upgrade prerender dependency

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 //@ts-check
-const prerender = require('prerender')
-
+const prerender = require('prerender');
 const server = prerender({
   followRedirects: true,
   chromeLocation: '/usr/bin/google-chrome',
-  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
-})
-server.use(require('prerender-memory-cache'))
-server.start()
+  chromeFlags: [
+    '--no-sandbox',
+    '--headless',
+    '--disable-gpu',
+    '--remote-debugging-port=9222',
+    '--hide-scrollbars',
+  ],
+});
+server.start();
